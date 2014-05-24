c5f3f33e327f62f8ee7ac040636e3fd3b3e3ff32
Hannes Widmoser <widmoser@gmail.com>
2014-05-24 21:53:45 +0200

    added pixi and requirejs libraries

---
+++ src/config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by widmoser on 24/05/14.
+ */
+requirejs.config({
+    shim: {
+
+    },
+    paths: {
+        pixi: "../lib/bower_components/pixi/bin/pixi",
+        requirejs: "../lib/bower_components/requirejs/require"
+    },
+    packages: [
+
+    ]
+});
