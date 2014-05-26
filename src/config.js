9145e229119994e3b5f3c9bdbf7def19c7c3bd48
Hannes Widmoser <widmoser@gmail.com>
2014-05-26 22:42:58 +0200

    setup requirejs and pixi

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ requirejs.config({
     shim: {
 
     },
+    baseUrl: '../src',
     paths: {
         pixi: "../lib/bower_components/pixi/bin/pixi",
         requirejs: "../lib/bower_components/requirejs/require"
@@ -13,3 +14,12 @@ requirejs.config({
 
     ]
 });
+
+// Start the main app logic.
+requirejs(['pixi', 'renderer/pixi'],
+    function (PIXI, renderer) {
+        var r = new renderer.PixiRenderer(document.getElementById("view"), 800, 600);
+
+
+    }
+);
--- src/renderer/pixi.js
+++ src/renderer/pixi.js
@@ -2,31 +2,60 @@
  * Created by widmoser on 24/05/14.
  */
 
-/**
- * The WebGL renderer draws to a WebGL context.
- * @constructor
- */
-function PixiRenderer() {
-
-    Staff.prototype.draw = function(gobj) {
-
-    }
+define(["renderer/gscore", "pixi"], function(gscore, pixi) {
 
     /**
-     * Renders a graphical score
-     * @param target The canvas element to draw on
-     * @param gscore The graphical representation of the score
-     * @param options A set of options that define parameters for drawing.
+     * The WebGL renderer draws to a WebGL context.
+     * @constructor
      */
-    this.render = function(target, gscore, options) {
-        gscore.objects.forEach(function(gobj) {
-            this.draw(gobj);
-        });
+    function PixiRenderer(view, width, height, backgroundColor) {
+
+        if (!backgroundColor)
+            backgroundColor = 0x66FF99;
+
+        // create an new instance of a pixi stage
+        var stage = new pixi.Stage(backgroundColor);
+
+        // create a renderer instance.
+        var renderer = pixi.autoDetectRenderer(width, height, view);
+
+        requestAnimFrame(animate);
+
+        function animate() {
+
+            requestAnimFrame(animate);
+
+            // render the stage
+            renderer.render(stage);
+
+        }
+
+        gscore.Staff.prototype.draw = function(gobj) {
+
+        }
+
+        /**
+         * Renders a graphical score
+         * @param target The canvas element to draw on
+         * @param gscore The graphical representation of the score
+         * @param options A set of options that define parameters for drawing.
+         */
+        this.render = function(target, score, options) {
+            score.objects.forEach(function(gobj) {
+                this.draw(gobj);
+            });
+        }
+
+        this.draw = function(gobj) {
+            gobj
+        }
+
     }
 
-    this.draw = function(gobj) {
-        gobj
+    return {
+        PixiRenderer : PixiRenderer
     }
 
-}
+});
+
 
